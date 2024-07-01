9eaabfc28541e7226cd53de51d187329599599c5
sachin-gurung <saching061@hotmail.com>
2024-07-01 14:56:28 -0700

    Initial Commit

---
+++ MVVMDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVMDemo
+//
+//  Created by Sachin Gurung on 7/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVMDemo/MVVMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMDemoApp.swift
+//  MVVMDemo
+//
+//  Created by Sachin Gurung on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
